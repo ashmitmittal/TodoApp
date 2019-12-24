b85732856313f76f26b91261e23352faae524708
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-12-24 14:37:15 +0530

    v2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 var port = process.env.PORT || 4000;
 var bodyParser = require("body-parser");
@@ -8,6 +9,16 @@ var todoRoutes = require("./routes/todos");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/todos", todoRoutes);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+	// set static folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
 
